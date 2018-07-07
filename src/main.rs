4263740f39ada11181a95e8cf43054501b1571aa
Jan Szumiec <jan.szumiec@gmail.com>
2018-07-07 13:29:06 +0200

    Use chrono to do time operations.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate rumqtt;
+extern crate chrono;
 use rumqtt::{MqttOptions, MqttClient, QoS};
-use std::time::{SystemTime, Duration, UNIX_EPOCH};
+use std::time::Duration;
 use std::thread::sleep;
 use std::env;
+use chrono::prelude::*;
 
 fn main() {
     let mqtt_host = env::var("MQTT_HOST").expect("MQTT_HOST not set");
@@ -11,12 +13,13 @@ fn main() {
     let every_s :u64 = every_s.parse().expect("EVERY_SECONDS should be a number");
     
     let client_options = MqttOptions::new().set_broker(&format!("{}:{}", mqtt_host, mqtt_port));
+    let unix_epoch = Utc.ymd(1970, 1, 1).and_hms(0, 0, 0);
 
     let mut request = MqttClient::start(client_options, None).expect("can't start");
     loop {
-        let t = SystemTime::now();
-        let e = t.duration_since(UNIX_EPOCH).expect("time went backwards");
-        let seconds = e.as_secs();
+        let t = Local::now();
+        let e = t.signed_duration_since(unix_epoch);
+        let seconds = e.num_seconds();
         let message = format!("{}", seconds);
         println!("{}", seconds);
         request.publish("time/epoch", QoS::Level1, message.into_bytes()).expect("publish failure");
